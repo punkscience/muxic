f45bedc74b88c90a03c890a84cecbae8984c6259
Darryl Wright <darryl@punkscience.ca>
2024-11-20 00:31:27 -0400

    Modified to use musicmove lib

--- cmd/copy.go
+++ cmd/copy.go
@@ -4,11 +4,17 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"log"
 	"muxic/musicutils"
-	"path/filepath"
 
+	"os"
+
+	"github.com/punkscience/movemusic"
 	"github.com/spf13/cobra"
 )
+
+var destructive bool
 
 // copyCmd represents the copy command
 var copyCmd = &cobra.Command{
@@ -21,23 +27,52 @@ removes any special characters from the file names.`,
 		// Get the complete list of files from the source folder
 		sourceFolder := cmd.Flag("source").Value.String()
 		targetFolder := cmd.Flag("target").Value.String()
+
 		allFiles := musicutils.GetAllMusicFiles(sourceFolder)
 
 		// Print all the files
 		for _, file := range allFiles {
-			// Make a target path name out of the file tag
-			targetPathName := musicutils.GetTargetPathName(file)
-			targetPathName = filepath.Join(targetFolder, targetPathName)
-
-			println(file)
-			println(targetPathName)
-
-			// Check to see if the target file already exists
-			// If it does, skip the file
-			if musicutils.FileExists(targetPathName) {
-				println("File already exists, skipping...")
-				continue
+			if destructive {
+				fmt.Println("Moving file: ", file)
+			} else {
+				fmt.Println("Copying file: ", file)
 			}
+
+			resultFileName, err := movemusic.CopyMusic(file, targetFolder, true)
+
+			// Check if the file is the same as the result file
+			sameFile := resultFileName == file
+
+			if err != nil {
+				if err == movemusic.ErrFileExists {
+					fmt.Println("File already exists, skipping.")
+
+					if destructive && !sameFile {
+						// Delete the source file
+						fmt.Println("Deleting source file: ", file)
+						err := os.Remove(file)
+
+						if err != nil {
+							println("Error deleting file: ", err)
+						}
+					}
+				} else {
+					log.Println("Error copying file: ", err)
+				}
+
+				continue
+			} else if destructive && !sameFile {
+
+				// Delete the source file
+				fmt.Println("Deleting source file: ", file)
+				err := os.Remove(file)
+
+				if err != nil {
+					println("Error deleting file: ", err)
+				}
+			}
+
+			println("Finished: ", resultFileName)
 		}
 
 	},
@@ -54,6 +89,7 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	copyCmd.Flags().String("source", "s", "The source folder name")
-	copyCmd.Flags().String("target", "t", "The destination folder name")
+	copyCmd.Flags().String("source", "", "The source folder name")
+	copyCmd.Flags().String("target", "", "The destination folder name")
+	copyCmd.Flags().BoolVarP(&destructive, "move", "m", false, "Delete the source file after copying")
 }
--- musicutils/musicutils.go
+++ musicutils/musicutils.go
@@ -2,14 +2,9 @@ package musicutils
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/wtolson/go-taglib"
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
 )
 
 // GetAllMusicFiles returns a list of all music files in the specified folder
@@ -32,39 +27,6 @@ func GetAllMusicFiles(folder string) []string {
 		fmt.Printf("error walking the path %q: %v\n", folder, err)
 	}
 	return files
-}
-
-// GetTargetPathName returns the target path name for the file
-func GetTargetPathName(file string) string {
-	targetPath := ""
-
-	// Get the music file tag info
-	if strings.HasSuffix(file, ".mp3") || strings.HasSuffix(file, ".flac") || strings.HasSuffix(file, ".wav") {
-		tag, err := taglib.Read(file)
-		if err != nil {
-			fmt.Printf("error opening file %q: %v\n", file, err)
-			return targetPath
-		}
-		defer tag.Close()
-
-		// Format each string in proper title format
-		converter := cases.Title(language.English)
-		artist := converter.String(tag.Artist())
-		album := converter.String(tag.Album())
-		title := converter.String(tag.Title())
-		track := fmt.Sprintf("%d", tag.Track())
-
-		// Retrieve the desired tag information, e.g., tag.Title(), tag.Artist(), etc.
-		targetPath = fmt.Sprintf("%s/%s/%s - %s", artist, album, track, title)
-		targetPath = targetPath + filepath.Ext(file)
-
-		// Return the target path name
-		return targetPath
-	} else {
-		log.Println("Unsupported file type with extension: ", filepath.Ext(file))
-	}
-
-	return targetPath
 }
 
 // FileExists checks to see if the file exists
